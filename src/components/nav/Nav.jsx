efd7de9d2b38d638b76a87bc557e0ac4c60dcbce
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-04-24 02:45:37 +0800

    Done Floating Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,57 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {BsInfoSquare} from 'react-icons/bs'
-import {BsBookmarkCheck} from 'react-icons/bs'
-import {FaRegHandshake} from 'react-icons/fa'
-import {MdMailOutline} from 'react-icons/md'
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { BsInfoSquare } from "react-icons/bs";
+import { BsBookmarkCheck } from "react-icons/bs";
+import { FaRegHandshake } from "react-icons/fa";
+import { MdMailOutline } from "react-icons/md";
+import { useState } from "react";
 function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#"> <AiOutlineHome /></a>
-      <a href="#about"> <BsInfoSquare /></a>
-      <a href="#experience"> <BsBookmarkCheck /></a>
-      <a href="#services"> <FaRegHandshake /></a>
-      <a href="#contact"> <MdMailOutline /></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        {" "}
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        {" "}
+        <BsInfoSquare />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        {" "}
+        <BsBookmarkCheck />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        {" "}
+        <FaRegHandshake />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        {" "}
+        <MdMailOutline />
+      </a>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
